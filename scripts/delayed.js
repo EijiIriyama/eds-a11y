9ebecd159ff5e900a0a1b1108eb9ddcb584ba181
Ben Manter <33335223+yumeniiru@users.noreply.github.com>
2023-11-17 14:10:12 +0900

    push to data layer

--- scripts/delayed.js
+++ scripts/delayed.js
@@ -5,3 +5,22 @@ import { sampleRUM } from './aem.js';
 sampleRUM('cwv');
 
 // add more delayed functionality here
+
+// eslint-disable-next-line no-undef
+axe
+  .run()
+  .then((results) => {
+    console.log('Results: ', results);
+
+    // eslint-disable-next-line no-undef
+    if (adobeDataLayer) {
+      // eslint-disable-next-line no-undef
+      adobeDataLayer.push(results);
+    }
+    if (results.violations.length) {
+      throw new Error('Accessibility issues found');
+    }
+  })
+  .catch((err) => {
+    console.error('Something bad happened:', err.message);
+  });
